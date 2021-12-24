8ff9797b111a6eaecba3d8830224c87dd3b54a00
rafahuelin <rafahuelin@gmail.com>
2021-12-24 13:16:03 +0100

    add Header component

--- src/Pages/DetailPage.js
+++ src/Pages/DetailPage.js
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Header from '../Components/Header'
 
 
 const DetailPage = ({data}) => {
   const { id } = useParams()
   const mobile = data.find(el => el.id === id)
   return (
-    <div>Detail Page: {JSON.stringify(mobile)}</div>
+    <>
+      <Header />
+      <div>Detail Page: {JSON.stringify(mobile)}</div>
+    </>
+    
   )
 }
 
--- src/Pages/ListPage.js
+++ src/Pages/ListPage.js
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import ListItem from '../Components/ListItem'
+import Header from '../Components/Header'
 
 
 const ListPage = ({data}) => (
-  <div>
+  <>
+    <Header />
     <h1>ListPage</h1>
     <div>
       <ul>
         { data.map(mobile => <ListItem key={mobile.id} mobile={mobile} />) }
       </ul>
     </div>
-  </div>
+  </>
 )
 
 
