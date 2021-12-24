220e5d9c007e2a49dff695d7d1e4cb37860024af
rafahuelin <rafahuelin@gmail.com>
2021-12-24 21:36:20 +0100

    add formated header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <ul className="flex px-10 py-5">
+      <li className="mr-6">
+        <a className="text-teal-500 hover:text-teal-800" href="#">Mobile Shop</a>
+      </li>
+    </ul>
   )
 }
 
