74a19c2150fedc9e2995005571ef1e181d4c9881
rafahuelin <rafahuelin@gmail.com>
2021-12-24 21:42:49 +0100

    add navigation to home when clicking on logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <ul className="flex px-10 py-5">
       <li className="mr-6">
-        <a className="text-teal-500 hover:text-teal-800" href="#">Mobile Shop</a>
+        <a className="text-teal-500 hover:text-teal-800" href="/">Mobile Shop</a>
       </li>
     </ul>
   )
