6baed35e78b98c1f3cd74eb33d6cf371114c7d3d
rafahuelin <rafahuelin@gmail.com>
2021-12-25 21:42:03 +0100

    add cache system for ListPage

--- src/Pages/ListPage.js
+++ src/Pages/ListPage.js
@@ -1,43 +1,82 @@
 import PropTypes from 'prop-types'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../Components/Header'
 import ListItem from '../Components/ListItem'
 import Search from '../Components/Search'
 
 
-const ListPage = () => {
-  const data = [
-    {'id':'ZmGrkLRPXOTpxsU4jjAcv','brand':'Acer','model':'Iconia Talk S','price':'170','imgUrl':'https://front-test-api.herokuapp.com/images/ZmGrkLRPXOTpxsU4jjAcv.jpg'},
-    {'id':'cGjFJlmqNPIwU59AOcY8H','brand':'Acer','model':'Liquid Z6 Plus','price':'250','imgUrl':'https://front-test-api.herokuapp.com/images/cGjFJlmqNPIwU59AOcY8H.jpg'},
-    {'id':'8hKbH2UHPM_944nRHYN1n','brand':'Acer','model':'Liquid Z6','price':'120','imgUrl':'https://front-test-api.herokuapp.com/images/8hKbH2UHPM_944nRHYN1n.jpg'}
-  ]
+const EXPIRATION_TIME_MS = 60 * 60 * 1000 // 1h
+const PRODUCTS_URL = 'https://front-test-api.herokuapp.com/api/product'
 
-  const [filteredData, setFilteredData] = useState(data)
+const isCacheExpired = () => {
+  try {
+    const timestamp = JSON.parse(localStorage.getItem('mobilesTimestamp'))
+    const timeNow = new Date().getTime()
+    const timeDifference = timeNow - timestamp
+    if (isNaN(timeDifference)) throw new Error('time difference is NaN')
+    return timeNow - timestamp > EXPIRATION_TIME_MS
+  } catch (error) {
+    console.error(`Error comparing cache expiration: ${error}`)
+    localStorage.removeItem('mobiles')
+    return true
+  }
+}
+
+const ListPage = () => {
+  const [mobiles, setMobiles] = useState([])
+  const [filteredMobiles, setFilteredMobiles] = useState([])
+
+  const setCache = (data) => {
+    localStorage.setItem('mobiles', JSON.stringify(data))
+    const timeNow = new Date().getTime()
+    localStorage.setItem('mobilesTimestamp', timeNow)
+  }
+  
+  const fetchFromCache = () => {
+    const jsonData = JSON.parse(localStorage.getItem('mobiles'))
+    setMobiles(jsonData)
+    setFilteredMobiles(jsonData)
+    setCache(jsonData)
+  }
+  
+  const fetchFromAPI = async() => {
+    const data = await fetch(PRODUCTS_URL)
+    const jsonData = await data.json()
+    setMobiles(jsonData)
+    setFilteredMobiles(jsonData)
+    setCache(jsonData)
+  }
+
+  useEffect(() => {
+    isCacheExpired() ? fetchFromAPI() : fetchFromCache()
+  }, [])
 
   const handleSearch = (event) => {
     const searchValue = event.target.value.toLowerCase()
-    searchValue !== '' && setFilteredData(data.filter(mobile => {
+    searchValue !== '' && setFilteredMobiles(mobiles.filter(mobile => {
       const brand = mobile.brand.toLowerCase()
       const model = mobile.model.toLowerCase()
       const isMatch = brand.includes(searchValue) || model.includes(searchValue)
       return isMatch
     }))
 
-    searchValue === '' && setFilteredData(data)
+    searchValue === '' && setFilteredMobiles(mobiles)
   }
 
-  return <div className='relative'>
-    <div className="px-0 sm:px-5 md:px-10 items-stretch">
-      <Header />
-      <div className='min-w-min'>
-        <Search handleSearch={handleSearch} />
-        <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 grow">
-          { filteredData.map(mobile => <ListItem key={mobile.id} mobile={mobile} />) }
+  return (
+    <div className='relative'>
+      <div className="px-0 sm:px-5 md:px-10 items-stretch">
+        <Header />
+        <div className='min-w-min'>
+          <Search handleSearch={handleSearch} />
+          <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 grow">
+            { filteredMobiles.map(mobile => <ListItem key={mobile.id} mobile={mobile} />) }
+          </div>
         </div>
+        
       </div>
-      
     </div>
-  </div>
+  )
 }
 
 
