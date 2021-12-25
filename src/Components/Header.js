63551a3d3f9ee4a5dd630751480efada43486d22
rafahuelin <rafahuelin@gmail.com>
2021-12-25 23:09:42 +0100

    Cart placeholder and replace doble quotes by simple quotes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,10 +3,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <ul className="flex px-10 py-5">
-      <li className="mr-6">
-        <a className="text-teal-500 hover:text-teal-800" href="/">Mobile Shop</a>
+    <ul className='flex justify-between px-10 py-5'>
+      <li className='mr-6'>
+        <a className='text-teal-500 hover:text-teal-800' href='/'>Mobile Shop</a>
       </li>
+      <li className='text-teal-500 hover:text-teal-800'>Cart 0</li>
     </ul>
   )
 }
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -7,8 +7,8 @@ const Search = ({handleSearch}) => {
   return (
     <>
       <div className='float-right px-10'>
-        <input type="search" className="border-2 border-teal-200 rounded p-3" placeholder="Search by brand or model..." onChange={handleSearch} />
-        <div className="absolute pin-r pin-t px-10 mt-3 mr-4 text-purple-lighter">
+        <input type='search' className='border-2 border-teal-200 rounded p-3' placeholder='Search by brand or model...' onChange={handleSearch} />
+        <div className='absolute pin-r pin-t px-10 mt-3 mr-4 text-purple-lighter'>
 
         </div>
       </div>
