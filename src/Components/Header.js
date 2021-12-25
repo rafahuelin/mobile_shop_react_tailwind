d43d17502ae2975fee51563f2a3391e027ced0cd
rafahuelin <rafahuelin@gmail.com>
2021-12-25 23:22:05 +0100

    cart placeholder

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 
 const Header = () => {
+  const cartValue = localStorage.getItem('cart') || '0'
+
   return (
     <ul className='flex justify-between px-10 py-5'>
       <li className='mr-6'>
         <a className='text-teal-500 hover:text-teal-800' href='/'>Mobile Shop</a>
       </li>
-      <li className='text-teal-500 hover:text-teal-800'>Cart 0</li>
+      <li className='text-teal-500 hover:text-teal-800'>Cart {cartValue}</li>
     </ul>
   )
 }
